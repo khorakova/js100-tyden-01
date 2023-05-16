707bdf613a0408a45f52f3bcc18794354584dbdd
Luděk Roleček <ludek@rolecek.cz>
2023-05-16 13:38:06 +0200

    Aktualizace podkladů

---
+++ uvod-a-instalace/prvni-stranka/script.js
@@ -0,0 +1,23 @@
+// toto je radkovy komentar
+console.log("Dalsi radek"); // toto vypisuje text do konzole
+
+/*
+	Toto muze byt komentar,
+	ktery je na vice
+	radku
+*/
+
+// console.log("JavaScript funguje");
+
+/*
+Ctrl+K Ctlr+C   →  pridat komentar
+Ctrl+K Ctlr+U   →  odebrat komentar
+Ctrl+/          →  zapnout/vypnout komentar
+*/
+
+
+// console.log("Ahoj");
+// console.log("Jsem Ludek");
+// console.log("Nemam moc vlasu");
+
+console.log("Ale jsem happy");
